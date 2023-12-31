bfa4686369093f9b0563286d11fda3dd99340a0f
cchoirun <niamchoirun020#gmail.com>
2023-12-31 19:15:38 +0700

    Add Covariant.kt

---
+++ belajar-kotlin-generic/src/app/Covariant.kt
@@ -0,0 +1,22 @@
+package app
+
+class Covariant<out T>(val data: T){
+
+    fun data(): T = data
+
+    // Tidak boleh membuat function didalam covariant
+//    fun setData(param: T){
+//        data = param
+//    }
+}
+
+fun main() {
+    val covariantString = Covariant<String>("Niam")
+    val covariantAny: Covariant<Any> = covariantString
+// Hal diatas boleh karena meskipun merubah ke parentnya yakni Any namun cuma dapat mengambil data, tanpa merubah
+
+    println(covariantAny.data)
+
+//   covariantAny.setData(100)  // -> tidak boleh karena merubah type data
+
+}
